4f4d616ed14ccd0c16ec252bbc7c4a0fa64fe907
Seunghwa-Han <22000786@handong.ac.kr>
2022-08-01 11:16:07 +0900

    create get_board func

--- ai.py
+++ ai.py
@@ -9,17 +9,12 @@ def main():
 	ip = input("input ip: ")
 	port = int(input("input port number: "))
 	ai_home = input("input BLACK or WHITE: ")
-
-    if ai_home == "BLACK":
-        ai_home = 'B'
-    else:
-        ai_home = 'W'
 	
-	red_stones = connsix.lets_connect(ip, port, dummy_home)
+	red_stones = connsix.lets_connect(ip, port, ai_home)
 	if len(red_stones):	
 		print("Received red stones from server: " + red_stones)
 
-	if ai_home == 'B':
+	if ai_home == 'BLACK':
 		away_move = connsix.draw_and_read("K10")
 		print("Received first away move from server: " + away_move)
 	else:
@@ -28,7 +23,6 @@ def main():
 	while 1:
 		away_move = connsix.draw_and_read(utils.make_move())
 		print("Received away move from server: " + away_move)
-
 
 if __name__ == "__main__":
 	main()
--- utils.py
+++ utils.py
@@ -2,7 +2,34 @@
 
 '''
 from CONNSIX import connsix
+import ai 
 import random 
 
 def make_move():
 	return chr(random.randint(65, 84)) + str(random.randint(1, 19)) + ":" + chr(random.randint(65, 84)) + str(random.randint(1, 19))
+
+def get_board(ai_home):
+    # A~T (I 제외)
+    # 1~19
+    board = [[0 for i in range(19)] for j in range(19)]
+
+    alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't']
+
+    for i in alphabets:
+        for j in range(1,20):
+            ret = connsix.get_stone_at(i+str(j))
+            x = ord(i) - 97
+            y = 19 - j
+            if x > 8:
+                x = x - 1
+            if ret == 'N':
+                continue
+            elif ret == 'E':
+                board[y][x] = 0 
+            elif ret == 'R':
+                board[y][x] = 3 
+            elif ret == ai_home[0]:
+                board[y][x] = 1 
+            else:    
+                board[y][x] = -1 
+    return board 
