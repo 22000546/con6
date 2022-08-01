21d6d3f2ff48af321f8628e0712cc28f12ec5105
22000546 <leeyan413@naver.com>
2022-08-01 23:24:58 +0900

    modify make_move func

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 '''
 
 '''
+from regex import B
 from CONNSIX import connsix
-import ai 
-import random 
+import algo1
+import algo7
 
 last_ai_move = None #[(x,y)] list
 last_away_move = None
@@ -15,19 +16,47 @@ def set_ai_move(ai_move):
 def set_away_move(away_move):
 	global last_away_move
 	last_away_move = coor_to_num(away_move)
+ 
+def get_ai_move():
+	global last_ai_move
+	return last_ai_move
+
+def get_away_move():
+	global last_away_move
+	return last_away_move    
 
 def make_move(board):
 	global last_ai_move, last_away_move
-	print("Last my points: ", num_to_coor(last_ai_move))
-	print("Last your points: ", num_to_coor(last_away_move))
+	left = 2
+ 
 	# 1. 내가 끝낼 수 있는 경우
+	result = algo1.find_5stones_open(1, board, left)
+	final_result = result
+	left -= len(result)
+	result = algo1.find_4stones_open(1, board, left)
+	final_result += result
+	left -= len(result)
 	# 2. 내가 무조건 막아야 하는 경우
+	result = algo1.find_5stones_open(10, board, left)
+	final_result += result
+	left -= len(result)
+	result = algo1.find_4stones_open(10, board, left)
+	final_result += result
+	left -= len(result)
 	# 3. 상대가 돌 2개를 사용하도록 공격하는 경우
 	# 4. 상대가 돌 1개를 사용하도록 공격하는 경우
-	# 5. 아무것도 할 게 없는 경우 (랜덤)
-	result = find_random(board)
-	last_ai_move = result
-	return num_to_coor(result)
+	# 5. 방어하는 경우
+	# 6. 아무것도 할 게 없는 경우 (코너 -> 랜덤)
+	result = algo7.find_corner(board, left)
+	final_result += result
+	left -= len(result)
+	result = algo7.find_random(1, board, left)
+	final_result += result
+	left -= len(result)
+ 
+	last_ai_move = final_result
+	print(last_ai_move)
+	return num_to_coor(final_result)
 
 def num_to_coor(lst):
 	if lst is None:
@@ -75,33 +104,9 @@ def get_board(ai_home):
             elif ret == 'E':
                 board[y][x] = 0 
             elif ret == 'R':
-                board[y][x] = 3 
+                board[y][x] = 100
             elif ret == ai_home[0]:
                 board[y][x] = 1 
             else:    
-                board[y][x] = -1 
-    return board 
-
-def find_4stones_open(stone, board):
-	global last_ai_move, last_away_move
-
-	lst = []
-
-	return lst
-
-def find_random(board):
-	lst = []
-	x1 = random.randint(0, 18)
-	y1 = random.randint(0, 18)
-	while board[y1][x1] != 0:
-		x1 = random.randint(0, 18)
-		y1 = random.randint(0, 18)
-	lst.append((x1, y1))
-
-	x2 = random.randint(0, 18)
-	y2 = random.randint(0, 18)
-	while board[y2][x2] != 0:
-		x2 = random.randint(0, 18)
-		y2 = random.randint(0, 18)
-	lst.append((x2, y2))
-	return lst
+                board[y][x] = 10
+    return board
