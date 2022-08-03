779dfca4a7fe950685bfc8f5966028d5f5e49356
Seunghwa-Han <22000786@handong.ac.kr>
2022-08-03 18:00:31 +0900

    modify open3 in attack2

--- attack2.py
+++ attack2.py
@@ -44,118 +44,279 @@ def open3(stone, board, my_last_points):
 			if(board[last_y][last_x-2]==0 and board[last_y][last_x-1]==0):
 				if(board[last_y][last_x+1] + board[last_y][last_x+2] + board[last_y][last_x+3] == 2*stone):
 					if(board[last_y][last_x+4] == 0 and board[last_y][last_x+5] == 0):
-						for i in range(last_x, last_x+4):
-							if board[last_y][i] == 0:
-								candidate.add((i, last_y))
+						if stone == 1:
+							for i in range(last_x, last_x+4):
+								if board[last_y][i] == 0:
+									candidate.add((i, last_y))
+						elif stone == 10:
+							# 상대방 돌 주위 2칸 이내 
+							for i in range(last_x, last_x+4):
+								if board[last_y][i] == stone:
+									temp.append((i, last_y))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y][x+j] == 0:
+										candidate.add((x+j, y))
+							temp.clear()
 		if(last_x >= 3 and last_x <= 14):
 			if(board[last_y][last_x-3]==0 and board[last_y][last_x-2]==0):
 				if(board[last_y][last_x-1] + board[last_y][last_x+1] + board[last_y][last_x+2] == 2*stone):
 					if(board[last_y][last_x+3] == 0 and board[last_y][last_x+4] == 0):
-						for i in range(last_x-1, last_x+3):
-							if board[last_y][i] == 0:
-								candidate.add((i, last_y))
+						if stone == 1:
+							for i in range(last_x-1, last_x+3):
+								if board[last_y][i] == 0:
+									candidate.add((i, last_y))
+						elif stone == 10:
+							for i in range(last_x-1, last_x+3):
+								if board[last_y][i] == stone:
+									temp.append((i, last_y))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y][x+j] == 0:
+										candidate.add((x+j, y))
+							temp.clear()
 		if(last_x >= 4 and last_x <= 15):
 			if(board[last_y][last_x-4]==0 and board[last_y][last_x-3]==0):
 				if(board[last_y][last_x-2] + board[last_y][last_x-1] + board[last_y][last_x+1] == 2*stone):
 					if(board[last_y][last_x+2] == 0 and board[last_y][last_x+3] == 0):
-						for i in range(last_x-2, last_x+2):
-							if board[last_y][i] == 0:
-								candidate.add((i, last_y))
+						if stone == 1:
+							for i in range(last_x-2, last_x+2):
+								if board[last_y][i] == 0:
+									candidate.add((i, last_y))
+						elif stone == 10:
+							for i in range(last_x-2, last_x+2):
+								if board[last_y][i] == stone:
+									temp.append((i, last_y))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y][x+j] == 0:
+										candidate.add((x+j, y))
+							temp.clear()
 		if(last_x >= 5 and last_x <= 16):
 			if(board[last_y][last_x-5]==0 and board[last_y][last_x-4]==0):
 				if(board[last_y][last_x-3] + board[last_y][last_x-2] + board[last_y][last_x-1] == 2*stone):
 					if(board[last_y][last_x+1] == 0 and board[last_y][last_x+2] == 0):
-						for i in range(last_x-3, last_x+1):
-							if board[last_y][i] == 0:
-								candidate.add((i, last_y))
+						if stone == 1:
+							for i in range(last_x-3, last_x+1):
+								if board[last_y][i] == 0:
+									candidate.add((i, last_y))
+						elif stone == 10:
+							for i in range(last_x-3, last_x+1):
+								if board[last_y][i] == stone:
+									temp.append((i, last_y))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y][x+j] == 0:
+										candidate.add((x+j, y))
+							temp.clear()
 		# |방향
 		if(last_y >= 2 and last_y <= 13):
 			if(board[last_y-2][last_x]==0 and board[last_y-1][last_x]==0):
 				if(board[last_y+1][last_x] + board[last_y+2][last_x] + board[last_y+3][last_x] == 2*stone):
 					if(board[last_y+4][last_x] == 0 and board[last_y+5][last_x] == 0):
-						for i in range(last_y, last_y+4):
-							if board[i][last_x] == 0:
-								candidate.add((last_x, i))
+						if stone == 1:
+							for i in range(last_y, last_y+4):
+								if board[i][last_x] == 0:
+									candidate.add((last_x, i))
+						elif stone == 10:
+							for i in range(last_y, last_y+4):
+								if board[i][last_x] == stone:
+									temp.append((last_x, i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x] == 0:
+										candidate.add((x, y+j))
+							temp.clear()
 		if(last_y >= 3 and last_y <= 14):
 			if(board[last_y-3][last_x]==0 and board[last_y-2][last_x]==0):
 				if(board[last_y-1][last_x] + board[last_y+1][last_x] + board[last_y+2][last_x] == 2*stone):
 					if(board[last_y+3][last_x] == 0 and board[last_y+4][last_x] == 0):
-						for i in range(last_y-1, last_y+3):
-							if board[i][last_x] == 0:
-								candidate.add((last_x, i))
+						if stone == 1:
+							for i in range(last_y-1, last_y+3):
+								if board[i][last_x] == 0:
+									candidate.add((last_x, i))
+						elif stone == 10:
+							for i in range(last_y-1, last_y+3):
+								if board[i][last_x] == stone:
+									temp.append((last_x, i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x] == 0:
+										candidate.add((x, y+j))
+							temp.clear()
 		if(last_y >= 4 and last_y <= 15):
 			if(board[last_y-4][last_x]==0 and board[last_y-3][last_x]==0):
 				if(board[last_y-2][last_x] + board[last_y-1][last_x] + board[last_y+1][last_x] == 2*stone):
 					if(board[last_y+2][last_x] == 0 and board[last_y+3][last_x] == 0):
-						for i in range(last_y-2, last_y+2):
-							if board[i][last_x] == 0:
-								candidate.add((last_x, i))
+						if stone == 1:
+							for i in range(last_y-2, last_y+2):
+								if board[i][last_x] == 0:
+									candidate.add((last_x, i))
+						elif stone == 10:
+							for i in range(last_y-2, last_y+2):
+								if board[i][last_x] == stone:
+									temp.append((last_x, i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x] == 0:
+										candidate.add((x, y+j))
+							temp.clear()
 		if(last_y >= 5 and last_y <= 16):
 			if(board[last_y-5][last_x]==0 and board[last_y-4][last_x]==0):
 				if(board[last_y-3][last_x] + board[last_y-2][last_x] + board[last_y-1][last_x] == 2*stone):
 					if(board[last_y+1][last_x] == 0 and board[last_y+2][last_x] == 0):
-						for i in range(last_y-3, last_y+1):
-							if board[i][last_x] == 0:
-								candidate.add((last_x, i))
+						if stone == 1:
+							for i in range(last_y-3, last_y+1):
+								if board[i][last_x] == 0:
+									candidate.add((last_x, i))
+						elif stone == 10:
+							for i in range(last_y-3, last_y+1):
+								if board[i][last_x] == stone:
+									temp.append((last_x, i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x] == 0:
+										candidate.add((x, y+j))
+							temp.clear()
 		# \방향
 		if(last_x >= 2 and last_x <= 13 and last_y >=2 and last_y <=13):
 			if(board[last_y-2][last_x-2]==0 and board[last_y-1][last_x-1]==0):
 				if(board[last_y+1][last_x+1] + board[last_y+2][last_x+2] + board[last_y+3][last_x+3] == 2*stone):
 					if(board[last_y+4][last_x+4] == 0 and board[last_y+5][last_x+5] == 0):
-						for i in range(0, 4):
-							if board[last_y+i][last_x+i] == 0:
-								candidate.add((last_x+i, last_y+i))
+						if stone == 1:
+							for i in range(0, 4):
+								if board[last_y+i][last_x+i] == 0:
+									candidate.add((last_x+i, last_y+i))
+						elif stone == 10:
+							for i in range(0,4):
+								if board[last_y+i][last_x+i] == stone:
+									temp.append((last_x+i, last_y+i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x+j] == 0:
+										candidate.add((x+j, y+j))
+							temp.clear()
 		if(last_x >= 3 and last_x <= 14 and last_y >=3 and last_y <=14):
 			if(board[last_y-3][last_x-3]==0 and board[last_y-2][last_x-2]==0):
 				if(board[last_y-1][last_x-1] + board[last_y+1][last_x+1] + board[last_y+2][last_x+2] == 2*stone):
 					if(board[last_y+3][last_x+3] == 0 and board[last_y+4][last_x+4] == 0):
-						for i in range(-1, 3):
-							if board[last_y+i][last_x+i] == 0:
-								candidate.add((last_x+i, last_y+i))
+						if stone == 1:
+							for i in range(-1, 3):
+								if board[last_y+i][last_x+i] == 0:
+									candidate.add((last_x+i, last_y+i))
+						elif stone == 10:
+							for i in range(-1, 3):
+								if board[last_y+i][last_x+i] == stone:
+									temp.append((last_x+i, last_y+i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x+j] == 0:
+										candidate.add((x+j, y+j))
+							temp.clear()
 		if(last_x >= 4 and last_x <= 15 and last_y >=4 and last_y <=15):
 			if(board[last_y-4][last_x-4]==0 and board[last_y-3][last_x-3]==0):
 				if(board[last_y-2][last_x-2] + board[last_y-1][last_x-1] + board[last_y+1][last_x+1] == 2*stone):
 					if(board[last_y+2][last_x+2] == 0 and board[last_y+3][last_x+3] == 0):
-						for i in range(-2, 2):
-							if board[last_y+i][last_x+i] == 0:
-								candidate.add((last_x+i, last_y+i))
+						if stone == 1:
+							for i in range(-2, 2):
+								if board[last_y+i][last_x+i] == 0:
+									candidate.add((last_x+i, last_y+i))
+					elif stone == 10:
+							for i in range(-2, 2):
+								if board[last_y+i][last_x+i] == stone:
+									temp.append((last_x+i, last_y+i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x+j] == 0:
+										candidate.add((x+j, y+j))
+							temp.clear()
 		if(last_x >= 5 and last_x <= 16 and last_y >=5 and last_y <=16):
 			if(board[last_y-5][last_x-5]==0 and board[last_y-4][last_x-4]==0):
 				if(board[last_y-3][last_x-3] + board[last_y-2][last_x-2] + board[last_y-1][last_x-1] == 2*stone):
 					if(board[last_y+1][last_x+1] == 0 and board[last_y+2][last_x+2] == 0):
-						for i in range(-3, 1):
-							if board[last_y+i][last_x+i] == 0:
-								candidate.add((last_x+i, last_y+i))
+						if stone == 1:
+							for i in range(-3, 1):
+								if board[last_y+i][last_x+i] == 0:
+									candidate.add((last_x+i, last_y+i))
+						elif stone == 10:
+							for i in range(-3, 1):
+								if board[last_y+i][last_x+i] == stone:
+									temp.append((last_x+i, last_y+i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x+j] == 0:
+										candidate.add((x+j, y+j))
+							temp.clear()
 		# /방향
 		if(last_x >= 5 and last_x <= 16 and last_y >=2 and last_y <=13):
 			if(board[last_y-2][last_x+2]==0 and board[last_y-1][last_x+1]==0):
 				if(board[last_y+1][last_x-1] + board[last_y+2][last_x-2] + board[last_y+3][last_x-3] == 2*stone):
 					if(board[last_y+4][last_x-4] == 0 and board[last_y+5][last_x-5] == 0):
-						for i in range(0, 4):
-							if board[last_y+i][last_x-i] == 0:
-								candidate.add((last_x-i, last_y+i))
+						if stone == 1:
+							for i in range(0, 4):
+								if board[last_y+i][last_x-i] == 0:
+									candidate.add((last_x-i, last_y+i))
+						elif stone == 10:
+							for i in range(0,4):
+								if board[last_y+i][last_x-i] == stone:
+									temp.append((last_x-i, last_y+i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x-j] == 0:
+										candidate.add((x-j, y+j))
+							temp.clear()
 		if(last_x >= 4 and last_x <= 15 and last_y >=3 and last_y <=14):
 			if(board[last_y-3][last_x+3]==0 and board[last_y-2][last_x+2]==0):
 				if(board[last_y-1][last_x+1] + board[last_y+1][last_x-1] + board[last_y+2][last_x-2] == 2*stone):
 					if(board[last_y+3][last_x-3] == 0 and board[last_y+4][last_x-4] == 0):
-						for i in range(-1, 3):
-							if board[last_y+i][last_x-i] == 0:
-								candidate.add((last_x-i, last_y+i))
+						if stone == 1:
+							for i in range(-1, 3):
+								if board[last_y+i][last_x-i] == 0:
+									candidate.add((last_x-i, last_y+i))
+						elif stone == 10:
+							for i in range(-1, 3):
+								if board[last_y+i][last_x-i] == stone:
+									temp.append((last_x-i, last_y+i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x-j] == 0:
+										candidate.add((x-j, y+j))
+							temp.clear()
 		if(last_x >= 4 and last_x <= 14 and last_y >=4 and last_y <=14):
 			if(board[last_y-4][last_x+4]==0 and board[last_y-3][last_x+3]==0):
 				if(board[last_y-2][last_x+2] + board[last_y-1][last_x+1] + board[last_y+1][last_x-1] == 2*stone):
 					if(board[last_y+2][last_x-2] == 0 and board[last_y+3][last_x-3] == 0):
-						for i in range(-2, 2):
-							if board[last_y+i][last_x-i] == 0:
-								candidate.add((last_x-i, last_y+i))
+						if stone == 1:
+							for i in range(-2, 2):
+								if board[last_y+i][last_x-i] == 0:
+									candidate.add((last_x-i, last_y+i))
+						elif stone == 10:
+							for i in range(-2, 2):
+								if board[last_y+i][last_x-i] == stone:
+									temp.append((last_x-i, last_y+i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x-j] == 0:
+										candidate.add((x-j, y+j))
+							temp.clear()
 		if(last_x >= 2 and last_x <= 13 and last_y >=5 and last_y <=16):
 			if(board[last_y-5][last_x+5]==0 and board[last_y-4][last_x+4]==0):
 				if(board[last_y-3][last_x+3] + board[last_y-2][last_x+2] + board[last_y-1][last_x+1] == 2*stone):
 					if(board[last_y+1][last_x-1] == 0 and board[last_y+2][last_x-2] == 0):
-						for i in range(-3, 1):
-							if board[last_y+i][last_x-i] == 0:
-								candidate.add((last_x-i, last_y+i))
-								#board[last_y+i][last_x-i] = 1
+						if stone == 1:
+							for i in range(-3, 1):
+								if board[last_y+i][last_x-i] == 0:
+									candidate.add((last_x-i, last_y+i))
+									#board[last_y+i][last_x-i] = 1
+						elif stone == 10:
+							for i in range(-3, 1):
+								if board[last_y+i][last_x-i] == stone:
+									temp.append((last_x-i, last_y+i))
+							for (x,y) in temp:
+								for j in [-2,-1,1,2]:
+									if board[y+j][x-j] == 0:
+										candidate.add((x-j, y+j))
+							temp.clear()
 	if len(candidate):
 		print("open3 candidate : " + str(candidate))
 		return random.choice(list(candidate))
